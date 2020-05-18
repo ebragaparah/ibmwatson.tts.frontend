7a17cc1e3cc7da995b92734373b7b770f3ff9c11
Eduardo <ebfjunior@gmail.com>
2020-05-18 03:02:59 -0300

    reproduz som ao clicar no botão referente ao audio

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import './Comment.css';
 
 const Comment = props => {
-    return (
-            <div className="card">
-              <div className="content">
-              <div className="header">{`#${props.comment.id}`}</div>
-                <div className="description">
-                  {props.comment.content}
-                </div>
-              </div>
-              <div className="ui bottom attached button">
-                <i className="assistive listening systems icon"></i>
-                  Escutar
-              </div>
-            </div>
-            
-    );
+  const audioElement = useRef(null);
+
+  const onAudioPlay = () => {
+    audioElement.current.play();
+  };
+    
+  return (
+    <div className="card">
+      <div className="content">
+        <div className="header">{`#${props.comment.id}`}</div>
+        <div className="description">
+          {props.comment.content}
+        </div>
+        <div className="voice">
+          <audio
+            autoPlay=""
+            id={props.comment.id}
+            className="audio"
+            controls=""
+            type="audio/mpeg"
+            src={`http://localhost:8000/${props.comment.id}.mp3`}
+            ref={audioElement}>
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+      </div>
+      <div onClick={() => onAudioPlay()}
+        className="ui bottom attached button">
+        <i className="assistive listening systems icon"></i>
+        Escutar
+      </div>
+    </div>
+  );
 };
 
 export default Comment;
