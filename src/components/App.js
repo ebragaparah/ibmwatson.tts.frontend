e79ece44448507bfb4ee39e7d234eaaad3c3abbe
Eduardo <ebfjunior@gmail.com>
2020-05-18 22:03:27 -0300

    identação

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
     fetchComments();
   }, []);
 
-  const onSubmit = comment => setComments([...comments, comment]);
+    const onSubmit = comment => setComments([...comments, comment]);
   
   return (
     <div className="ui container app">
